025e0fae157c895dbe364f89aabc68ae32627403
FrankTII <frank.training2@gmail.com>
2022-12-12 13:25:15 -0600

    S6 Budget analysis for Marketing Expending

--- S6/postworkS6Xy.R
+++ S6/postworkS6Xy.R
@@ -28,8 +28,6 @@ Considera:
 - X2: Radio (Presupuesto de publicidad en Radio para el producto)
 - X3: Newspaper (Presupuesto de publicidad en Periódico para el producto)"
 
-# Desarrollo
-
 "Analisis de datos"
 df <- read.csv("https://raw.githubusercontent.com/beduExpert/Programacion-R-Santander-2022/main/Sesion-06/data/advertising.csv")
 class(df)   #Id tipo de dato
@@ -42,47 +40,54 @@ names(df);  #Etiquetas tags del objeto
 length(df); #Longitud del Objeto
 View(df);   #Data Viewer
 
-"Seleccion de datos a analizar y generacion de matriz de Correlaciones Covarianzas,etc"
-library(dplyr)
-df.select <- select(df, TV, Radio, Newspaper, Sales)
+"Seleccion de datos a analizar"
+complete.cases(df)
+X1<-df$TV
+X2<-df$Radio
+X3<-df$Newspaper
+Y<-df$Sales
+dfxy<-data.frame(X1,X2,X3,Y)
+View(dfxy)
+str(dfxy)
+
+"Sacando matrices de correlacion varianzas"
 ?round
 ?cor
-round(cor(df.select),4)  
-cor(df)
-round(cor(df),4)
-"Generando matrices de graficos de puntos"
-pairs(~ Sales + TV + Radio + Newspaper, 
-      data = df, gap = 0.4, cex.labels = 2)
+cor(dfxy)
+"Redondeando a 4 digitos la matriz"
+round(cor(dfxy),4)
+
+"generando Matrices de graficos de puntos "
+?pairs
+pairs(~ Y + X1 + X2 + X3, 
+      data = dfxy, gap = 0.4, cex.labels = 2)
 
 "Estimación por Mínimos Cuadrados Ordinarios (OLS)
 Y = beta0 + beta1*TV + beta2*Radio + beta3*NewSpaPER + e"
-?attach
-attach(df)
+attach(dfxy)
 
-"Modelo base"
-m1 <- lm(Sales ~ TV + Radio + Newspaper)
+m1 <- lm(Y ~ X1 + X2 + X3)
 summary(m1)
 
 "Para ello, planteamos el siguiente juego de hipótesis:
   H0: beta0 = beta1= beta2 = beta3 = 0
-  (Y = beta0 + beta1*TV + beta2*Radio + beta3*NewSpaPER + e
+  (Y = beta0 + beta1*X1 + beta2*X2 + beta3*X3 + e
 
   H0 no es verdad (AL MENOS UN COEFICIENTE ES DISTINTO DE 0) Newspaper
 
   De los resultados anteriores, podemos concluir que el coeficiente de la variable 
   Newspaper (X3) no es significativo. Probemos nuestro modelo sin incluir dicha variable:
-  Y = beta0 + beta1*TV + beta2*Radio  + e"
+  Y = beta0 + beta1*X1 + beta2*X2  + e"
 
 "Modelo final"  
-m2 <- update(m1, ~.-Newspaper)
+
+m2 <- update(m1, ~.-X3)
 summary(m2)
 
 "Hipótesis Final:
   H0: beta0 = beta1= beta2 = beta3 = 0
-   Y = beta0 + beta1*TV + beta2*Radio  + e
+   Y = beta0 + beta1*X1 + beta2*X2  + e
 
   H0  es verdad (Ningun coeficiente es Distinto de Cero)
 
   De los resultados anteriores, podemos concluir que el Es mas eficiente invertir el presupuesto total de ventas en Radio (X1) y Televisión (X2)"
-
-
